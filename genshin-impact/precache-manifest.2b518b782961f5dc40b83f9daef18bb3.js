ca3beca881dfe4eee6c8b2e7aba757170ad66197
LouaiB <44529286+LouaiB@users.noreply.github.com>
2020-10-17 11:34:18 +0300

    Genshin Impact Replica First Batch

---
+++ genshin-impact/precache-manifest.2b518b782961f5dc40b83f9daef18bb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d992a05bd40d693ffa08cd7da85b2bb8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f13c72a6ab7708144c69",
+    "url": "/static/css/main.fdb57a04.chunk.css"
+  },
+  {
+    "revision": "90ec28450ccb474fd934",
+    "url": "/static/js/2.79b271e7.chunk.js"
+  },
+  {
+    "revision": "4df4c58fd3db8af31000da1d75d059ea",
+    "url": "/static/js/2.79b271e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13c72a6ab7708144c69",
+    "url": "/static/js/main.a9fde67f.chunk.js"
+  },
+  {
+    "revision": "5c483ba36267cf1ba322",
+    "url": "/static/js/runtime-main.f201c0e8.js"
+  }
+]);
